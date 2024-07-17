db86500f181d271460f4c8a87dd4aa5d5a71af7c
Csillag Kristof <csillag@harmacolindor.com>
2024-07-17 09:35:15 +0200

    Styling changes

--- frontend/src/components/InputFields/InputFieldGroup.tsx
+++ frontend/src/components/InputFields/InputFieldGroup.tsx
@@ -2,6 +2,7 @@ import { FieldConfiguration } from './validation';
 import { FC } from 'react';
 import { InputField } from './InputField';
 import { InputFieldControls } from './useInputField';
+import classes from "./index.module.css"
 
 type InputFieldGroupProps = {
   fields: FieldConfiguration
@@ -12,7 +13,7 @@ export const InputFieldGroup: FC<InputFieldGroupProps> = ({ fields }) => {
     {fields.map(
       row => Array.isArray(row)
         ? (
-          <div className={"niceLineWide"}>
+          <div className={classes.fieldRow}>
             { row.map(field =>
               <InputField key={field.name} controls={field} />
             ) }
