5f7d8cad9dfc700200ab024582cb122102842e9e
Kristof Csillag <kristof@oasisprotocol.org>
2024-09-11 09:42:21 +0200

    Remove duplicate cache layer

--- frontend/src/hooks/useIPFSData.ts
+++ frontend/src/hooks/useIPFSData.ts
@@ -1,22 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Pinata } from '../utils/Pinata'
 
-const cache = new Map<string, any>()
-
 export const useIPFSData = (hash: string | undefined) => {
   const [isLoading, setIsLoading] = useState(false)
   const [data, setData] = useState<Uint8Array | undefined>()
   const [error, setError] = useState<string | undefined>()
 
   const fetchData = async (key: string) => {
-    console.log('Fetching', key, 'from Pinata')
     setIsLoading(true)
     setError(undefined)
     try {
-      const data = await Pinata.fetchData(key)
-      cache.set(key, data)
-      console.log('Storing new value. Cache now has', cache.size, 'elements.')
-      setData(data)
+      setData(await Pinata.fetchData(key))
     } catch {
       setError('Failed to fetch data from IPFS')
     } finally {
@@ -26,13 +20,7 @@ export const useIPFSData = (hash: string | undefined) => {
 
   useEffect(() => {
     if (!hash) return
-    const storedValue = cache.get(hash)
-    if (storedValue) {
-      console.log('Cache hit on', hash)
-      setData(storedValue)
-    } else {
-      void fetchData(hash)
-    }
+    void fetchData(hash)
   }, [hash])
 
   return {
