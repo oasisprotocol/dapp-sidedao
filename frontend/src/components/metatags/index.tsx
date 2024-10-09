eefda3135cda1d882e51f5d86ca63edb42426b50
Kristof Csillag <csillag@nolmecolindor.com>
2024-10-09 12:22:28 +0200

    Change thumbnail name to defeat caching

--- frontend/src/components/metatags/index.tsx
+++ frontend/src/components/metatags/index.tsx
@@ -5,6 +5,6 @@ export const defaultMetatags: ReactNode[] = [
   <meta key="twitter-key" name="twitter:card" content="summary_large_image" />,
   <meta key="twitter-desc" name="twitter:description" content={appDescription} />,
   <meta key="og-desc" property="og:description" content={appDescription} />,
-  <meta key="twitter-image" name="twitter:image" content={`${appRootUrl}/thumbnail.png`} />,
-  <meta key="og-image" property="og:image" content={`${appRootUrl}/thumbnail.png`} />,
+  <meta key="twitter-image" name="twitter:image" content={`${appRootUrl}/thumbnail2.png`} />,
+  <meta key="og-image" property="og:image" content={`${appRootUrl}/thumbnail2.png`} />,
 ]
